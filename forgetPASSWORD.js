8e0bd6c91d673eaac1a287c071c6d54f4ebde7e3
debbyedo <ddebby106@gmail.com>
2022-07-25 18:06:03 +0100

    update

--- forgetPASSWORD.js
+++ forgetPASSWORD.js
@@ -1,27 +1,93 @@
-const forgotPassword = (Email, callbacks = {}) => {
-    const { onLoad, onSuccess, onFailure } = callbacks
-    return dispatch => {
-     if (onLoad && typeof onLoad === 'function') onLoad()
-     const poolData = {
-      UserPoolId: userPoolId,
-      ClientId: clientId
-     }
-     const userPool = new CognitoUserPool(poolData)
-     const userData = {
-      Email: Email,
-      Pool: userPool
-     }
-     const cognitoUser = new CognitoUser(userData)
-     cognitoUser.forgotPassword({
-      onSuccess: (result) => {
-       if (onSuccess && typeof onSuccess === 'function') onSuccess()
-      },
-      onFailure: (error) => {
-       console.error(error.message)
-       console.error(error.stack)
-       if (onFailure && typeof onFailure === 'function') onFailure(error.message)
-      }
-     })
+
+const emailEl = document.querySelector('email');
+const form = document.querySelector('#forgetpassword');
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
     }
-   }
-   
+    return valid;
+};
+
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+
+
+const isRequired = value => value === '' ? false : true;
+const isBetween = (length, min, max) => length < min || length > max ? false : true;
+
+const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    // show the error message
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    // hide the error message
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+
+    e.preventDefault();
+    // validate forms
+    let isEmailValid = checkEmail();
+        // isPasswordValid = checkPassword();
+    
+    let isFormValid = isEmailValid ;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+
+    }
+});
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+// 
+form.addEventListener('input', debounce(function (e) {
+    switch (e.target.id) {
+        case 'email':
+            checkEmail();
+            break;
+       
+    }
+}));
