2d2d56a4f0a55d4931298612db587f1fd68c6036
debbyedo <ddebby106@gmail.com>
2022-08-04 11:48:21 +0100

    update

---
+++ forgetPASSWORD.js
@@ -0,0 +1,24 @@
+// Dom elements
+let form = document.querySelector("forgotPassword");
+let email = document.querySelector("#email");
+
+// What to do with inputs ?
+function handleInput() {
+  // Values from dom elements ( input )
+  let emailValue = email.value.trim();
+
+  // Checking for email
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "Email is not valid");
+  } else {
+    setSuccessFor(email);
+  }
+}
+
+// To check if email is valid or not ?
+function isEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
