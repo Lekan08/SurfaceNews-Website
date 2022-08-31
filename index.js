24d3d2a5c29f68d85bc7ffe77858ed8dcc726714
Oluwasegun Ajayi <aoluwasegun@atbtechsoft.com>
2022-08-31 13:12:27 +0100

    update

--- index.js
+++ index.js
@@ -92,7 +92,31 @@ form.addEventListener('submit', function (e) {
 
     }
 });
+function field_focus(field, email)
+  {
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+  }
 
+  function field_blur(field, email)
+  {
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+  }
+
+//Fade in dashboard box
+$(document).ready(function(){
+    $('.box').hide().fadeIn(1000);
+    });
+
+//Stop click event
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
 
 const debounce = (fn, delay = 500) => {
     let timeoutId;
